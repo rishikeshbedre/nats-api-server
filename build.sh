54db01d2fd26791bfd31fd26822437f2b313713f
rishikeshbedre <35272655+rishikeshbedre@users.noreply.github.com>
2020-01-30 12:46:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 #rm -rf *.tar
 
-docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t nats-api-server:0.0.1 .
+docker build --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" -t nats-api-server:0.0.1 .
 
 docker run -it -p 4222:4222 -p 6060:6060 nats-api-server:0.0.1
 
