78955012982cdffe861e8ff29443cb7d06c8ac1e
rishikeshbedre <35272655+rishikeshbedre@users.noreply.github.com>
2020-02-04 13:02:25 +0530

    Update password_test.go

--- util/password_test.go
+++ util/password_test.go
@@ -10,4 +10,10 @@ func TestGenHashPassowrd(t *testing.T){
 	if err != nil {
 		t.Errorf("Test failed, string '%s' supplied but failed", inp)
 	}
+	
+	inp2 := "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{};:.,~1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	_, err2 := GenHashPassword(inp2)
+	if err2 != nil {
+		t.Errorf("Test failed, string '%s' supplied but failed", inp2)
+	}
 }
