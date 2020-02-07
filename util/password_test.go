78714cbef654ffc62fa8afb2350358617c7cb33f
rishikeshbedre <rishikeshbedre@gmail.com>
2020-02-08 00:24:49 +0530

    modified test case

--- util/password_test.go
+++ util/password_test.go
@@ -1,19 +1,13 @@
 package util
- 
+
 import (
 	"testing"
 )
 
-func TestGenHashPassowrd(t *testing.T){
+func TestGenHashPassowrd(t *testing.T) {
 	inp := "abc"
 	_, err := GenHashPassword(inp)
 	if err != nil {
 		t.Errorf("Test failed, string '%s' supplied but failed", inp)
-	}
-	
-	inp2 := "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{};:.,~1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	_, err2 := GenHashPassword(inp2)
-	if err2 != nil {
-		t.Errorf("Test failed, string '%s' supplied but failed", inp2)
 	}
 }
